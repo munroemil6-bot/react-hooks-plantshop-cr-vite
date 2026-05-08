b604cd91375dd699c54b2b6f5b1c7d7d266fdcce
mylesmunroe <munroemil6@gmail.com>
2026-05-08 23:19:21 +0300

    shopin backend linkage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import Header from "./components/Header";
-import PlantPage from "./components/PlantPage";
+import React from "react";
+import Header from "./Header";
+import PlantPage from "./PlantPage";
 
 function App() {
   const [plants, setPlants] = useState([]);
